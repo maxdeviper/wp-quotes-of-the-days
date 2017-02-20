1d8ff714cb16b1140b0883a70fade00503f587d3
valentine <valentine>
2017-02-20 17:26:48 +0100

    mobile-responsive css tweak and quote after content

--- js/main.js
+++ js/main.js
@@ -2,26 +2,29 @@
 (function($){
 	$(document).ready(function(){
 		$('#wp-quotes').show();
-		$("body").addClass("quote-shown");
+		if ($('#wp-quotes').length > 0){
+			$("body").addClass("quote-shown");
+			var _countDownNumber = 0;
+			var countDown = function(countDownNumber){
+				if (countDownNumber >= 5){
+					$('#wp-quotes .countdown').hide(function(){
+						$('#wp-quotes .continue-button').css('display', 'inline-block');
+					});
+					return;
+				}
+				countDownNumber++;
+				$('#wp-quotes .countdown .lead span.count').text(countDownNumber);
+				setTimeout(countDown,1000, countDownNumber);
+			}
+			setTimeout(countDown, 1000, _countDownNumber);
+			$(document).on('click', '.continue-button', function(e){
+				$("body").removeClass("quote-shown");
+				$('#wp-quotes').fadeOut( "slow" );
+			});
+			
+		}
 		// setTimeout(function(){
 		// 	$('#wp-quotes').fadeOut( "slow" );
 		// }, 8000);
-		var _countDownNumber = 0;
-		var countDown = function(countDownNumber){
-			if (countDownNumber >= 5){
-				$('#wp-quotes .countdown').hide(function(){
-					$('#wp-quotes .continue-button').css('display', 'inline-block');
-				});
-				return;
-			}
-			countDownNumber++;
-			$('#wp-quotes .countdown .lead span.count').text(countDownNumber);
-			setTimeout(countDown,1000, countDownNumber);
-		}
-		setTimeout(countDown, 1000, _countDownNumber);
-		$(document).on('click', '.continue-button', function(e){
-			$("body").removeClass("quote-shown");
-			$('#wp-quotes').fadeOut( "slow" );
-		});
 	});
 })(jQuery);
--- wp-quote-display.php
+++ wp-quote-display.php
@@ -6,7 +6,7 @@
  * @wordpress-plugin
  * Plugin URI:        www.opushive.com
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           0.0.5
+ * Version:           0.0.6
  * Author:            Opus Hive
  * Author URI:        www.opushive.com
  * License:           GPL-2.0+
@@ -40,16 +40,43 @@ function opus_hive_display_quote(){
 
 }
 
+function opus_hive_display_quote_after_content($content){
+	$queryArgs = array(
+			'method' => 'getQuote',
+			'format' => 'json',
+			'lang'   => 'en'
+
+		);
+	$html = '';
+	$request = wp_remote_get( 'http://api.forismatic.com/api/1.0/?'.http_build_query($queryArgs));
+	if(is_wp_error( $request )) {
+        error_log('error in making request for quotes');
+        return '';
+     }
+    $response = json_decode( wp_remote_retrieve_body($request), true);
+    //die(var_dump($response));
+    if (!empty($response)){
+
+	    extract($response);
+		ob_start();
+		include plugin_dir_path( __FILE__ ).'/templates/quote-from-forbes.php';
+		$html = ob_get_clean();
+    }
+;	return $content.$html;
+
+}
+
 function opus_hive_quote_enqueue_style() {
-	wp_enqueue_style( 'opus_hive_quote_css', plugin_dir_url( __FILE__ ).'/css/main2.css'); 
+	wp_enqueue_style( 'opus_hive_quote_css', plugin_dir_url( __FILE__ ).'/css/main2.css',false, '0.0.7'); 
 	wp_enqueue_style( 'font-awesome', "https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"); 
 }
 
 function opus_hive_quote_enqueue_script() {
-	wp_enqueue_script( 'opus_hive_quote_js', plugin_dir_url( __FILE__ ).'/js/main.js', array( 'jquery' ));
+	wp_enqueue_script( 'opus_hive_quote_js', plugin_dir_url( __FILE__ ).'/js/main.js', array( 'jquery' ), '0.0.7');
 }
 
 
 add_shortcode( 'wp-quote-of-the-day', 'opus_hive_display_quote' );
 add_action( 'wp_enqueue_scripts', 'opus_hive_quote_enqueue_style' );
 add_action( 'wp_enqueue_scripts', 'opus_hive_quote_enqueue_script' );
+add_filter ('the_content', 'opus_hive_display_quote_after_content');
